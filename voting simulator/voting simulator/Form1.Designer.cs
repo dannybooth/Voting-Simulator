2402dcd2e06da9ee5fa7e70330ddc78b323ad67d
dannybooth <80790608+dannybooth@users.noreply.github.com>
2022-06-08 18:59:28 +0100

    Add files via upload

---
+++ voting simulator/voting simulator/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿
+namespace voting_simulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.fileUpload = new System.Windows.Forms.Button();
+            this.simulate = new System.Windows.Forms.Button();
+            this.data = new System.Windows.Forms.TextBox();
+            this.fileSystemWatcher = new System.IO.FileSystemWatcher();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.location = new System.Windows.Forms.Label();
+            this.inpuLabel = new System.Windows.Forms.Label();
+            this.dropdownmenu = new System.Windows.Forms.Panel();
+            this.pairwise = new System.Windows.Forms.Button();
+            this.method3 = new System.Windows.Forms.Button();
+            this.dropdown = new System.Windows.Forms.Button();
+            this.method2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.amountCheck = new System.Windows.Forms.CheckBox();
+            this.delimiterText = new System.Windows.Forms.TextBox();
+            this.delimiterLabel = new System.Windows.Forms.Label();
+            this.error = new System.Windows.Forms.Label();
+            this.matrixLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).BeginInit();
+            this.dropdownmenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileUpload
+            // 
+            this.fileUpload.Location = new System.Drawing.Point(21, 271);
+            this.fileUpload.Name = "fileUpload";
+            this.fileUpload.Size = new System.Drawing.Size(280, 35);
+            this.fileUpload.TabIndex = 0;
+            this.fileUpload.Text = "Upload file";
+            this.fileUpload.UseVisualStyleBackColor = true;
+            this.fileUpload.Click += new System.EventHandler(this.fileUpload_Click);
+            // 
+            // simulate
+            // 
+            this.simulate.Location = new System.Drawing.Point(310, 271);
+            this.simulate.Name = "simulate";
+            this.simulate.Size = new System.Drawing.Size(294, 35);
+            this.simulate.TabIndex = 1;
+            this.simulate.Text = "Simulate";
+            this.simulate.UseVisualStyleBackColor = true;
+            this.simulate.Click += new System.EventHandler(this.simulate_Click);
+            // 
+            // data
+            // 
+            this.data.Location = new System.Drawing.Point(18, 25);
+            this.data.Multiline = true;
+            this.data.Name = "data";
+            this.data.Size = new System.Drawing.Size(586, 210);
+            this.data.TabIndex = 2;
+            this.data.TextChanged += new System.EventHandler(this.data_TextChanged);
+            // 
+            // fileSystemWatcher
+            // 
+            this.fileSystemWatcher.EnableRaisingEvents = true;
+            this.fileSystemWatcher.SynchronizingObject = this;
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(21, 412);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(139, 13);
+            this.ResultLabel.TabIndex = 3;
+            this.ResultLabel.Text = "No data has been simulated";
+            // 
+            // location
+            // 
+            this.location.AutoSize = true;
+            this.location.Location = new System.Drawing.Point(21, 255);
+            this.location.Name = "location";
+            this.location.Size = new System.Drawing.Size(16, 13);
+            this.location.TabIndex = 4;
+            this.location.Text = "...";
+            // 
+            // inpuLabel
+            // 
+            this.inpuLabel.AutoSize = true;
+            this.inpuLabel.Location = new System.Drawing.Point(10, 5);
+            this.inpuLabel.Name = "inpuLabel";
+            this.inpuLabel.Size = new System.Drawing.Size(137, 13);
+            this.inpuLabel.TabIndex = 5;
+            this.inpuLabel.Text = "Test data for votes inputted";
+            // 
+            // dropdownmenu
+            // 
+            this.dropdownmenu.Controls.Add(this.pairwise);
+            this.dropdownmenu.Controls.Add(this.method3);
+            this.dropdownmenu.Controls.Add(this.dropdown);
+            this.dropdownmenu.Controls.Add(this.method2);
+            this.dropdownmenu.Location = new System.Drawing.Point(21, 312);
+            this.dropdownmenu.MaximumSize = new System.Drawing.Size(583, 96);
+            this.dropdownmenu.MinimumSize = new System.Drawing.Size(583, 35);
+            this.dropdownmenu.Name = "dropdownmenu";
+            this.dropdownmenu.Size = new System.Drawing.Size(583, 96);
+            this.dropdownmenu.TabIndex = 7;
+            // 
+            // pairwise
+            // 
+            this.pairwise.Location = new System.Drawing.Point(0, 74);
+            this.pairwise.Name = "pairwise";
+            this.pairwise.Size = new System.Drawing.Size(583, 23);
+            this.pairwise.TabIndex = 8;
+            this.pairwise.Text = "Pairwise";
+            this.pairwise.UseVisualStyleBackColor = true;
+            this.pairwise.Click += new System.EventHandler(this.pairwise_Click);
+            // 
+            // method3
+            // 
+            this.method3.Location = new System.Drawing.Point(0, 53);
+            this.method3.Name = "method3";
+            this.method3.Size = new System.Drawing.Size(583, 23);
+            this.method3.TabIndex = 3;
+            this.method3.Text = "Prefrence Order";
+            this.method3.UseVisualStyleBackColor = true;
+            this.method3.Click += new System.EventHandler(this.method3_Click);
+            // 
+            // dropdown
+            // 
+            this.dropdown.Location = new System.Drawing.Point(0, 0);
+            this.dropdown.Name = "dropdown";
+            this.dropdown.Size = new System.Drawing.Size(583, 35);
+            this.dropdown.TabIndex = 2;
+            this.dropdown.Text = "Methods";
+            this.dropdown.UseVisualStyleBackColor = true;
+            this.dropdown.Click += new System.EventHandler(this.dropdown_Click);
+            // 
+            // method2
+            // 
+            this.method2.Location = new System.Drawing.Point(0, 32);
+            this.method2.Name = "method2";
+            this.method2.Size = new System.Drawing.Size(583, 23);
+            this.method2.TabIndex = 1;
+            this.method2.Text = "First Choice";
+            this.method2.UseVisualStyleBackColor = true;
+            this.method2.Click += new System.EventHandler(this.method2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 15;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // amountCheck
+            // 
+            this.amountCheck.AutoSize = true;
+            this.amountCheck.Location = new System.Drawing.Point(328, 5);
+            this.amountCheck.Name = "amountCheck";
+            this.amountCheck.Size = new System.Drawing.Size(276, 17);
+            this.amountCheck.TabIndex = 8;
+            this.amountCheck.Text = "Is the amount of times a vote been casted listed first?";
+            this.amountCheck.UseVisualStyleBackColor = true;
+            // 
+            // delimiterText
+            // 
+            this.delimiterText.Location = new System.Drawing.Point(78, 237);
+            this.delimiterText.MaxLength = 1;
+            this.delimiterText.Multiline = true;
+            this.delimiterText.Name = "delimiterText";
+            this.delimiterText.Size = new System.Drawing.Size(18, 18);
+            this.delimiterText.TabIndex = 9;
+            this.delimiterText.TextChanged += new System.EventHandler(this.delimiterText_TextChanged);
+            // 
+            // delimiterLabel
+            // 
+            this.delimiterLabel.AutoSize = true;
+            this.delimiterLabel.Location = new System.Drawing.Point(18, 239);
+            this.delimiterLabel.Name = "delimiterLabel";
+            this.delimiterLabel.Size = new System.Drawing.Size(50, 13);
+            this.delimiterLabel.TabIndex = 10;
+            this.delimiterLabel.Text = "Delimiter:";
+            // 
+            // error
+            // 
+            this.error.AutoSize = true;
+            this.error.Location = new System.Drawing.Point(96, 240);
+            this.error.Name = "error";
+            this.error.Size = new System.Drawing.Size(0, 13);
+            this.error.TabIndex = 11;
+            // 
+            // matrixLabel
+            // 
+            this.matrixLabel.AutoSize = true;
+            this.matrixLabel.Location = new System.Drawing.Point(431, 412);
+            this.matrixLabel.Name = "matrixLabel";
+            this.matrixLabel.Size = new System.Drawing.Size(0, 13);
+            this.matrixLabel.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 635);
+            this.Controls.Add(this.matrixLabel);
+            this.Controls.Add(this.error);
+            this.Controls.Add(this.delimiterLabel);
+            this.Controls.Add(this.delimiterText);
+            this.Controls.Add(this.amountCheck);
+            this.Controls.Add(this.dropdownmenu);
+            this.Controls.Add(this.inpuLabel);
+            this.Controls.Add(this.location);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.data);
+            this.Controls.Add(this.simulate);
+            this.Controls.Add(this.fileUpload);
+            this.Name = "Form1";
+            this.Text = "Voting Simulatior";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).EndInit();
+            this.dropdownmenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button fileUpload;
+        private System.Windows.Forms.Button simulate;
+        private System.Windows.Forms.TextBox data;
+        private System.IO.FileSystemWatcher fileSystemWatcher;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Label location;
+        private System.Windows.Forms.Label inpuLabel;
+        private System.Windows.Forms.Panel dropdownmenu;
+        private System.Windows.Forms.Button method2;
+        private System.Windows.Forms.Button dropdown;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button method3;
+        private System.Windows.Forms.Button pairwise;
+        private System.Windows.Forms.CheckBox amountCheck;
+        private System.Windows.Forms.Label delimiterLabel;
+        private System.Windows.Forms.TextBox delimiterText;
+        private System.Windows.Forms.Label error;
+        private System.Windows.Forms.Label matrixLabel;
+    }
+}
+
---
+++ voting simulator/voting simulator/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace voting_simulator
+{
+    public partial class Form1 : Form
+    {
+        string filetext;
+        string[] fileline;
+        private bool isCollapsed = true;
+        int method = 1;
+        char delimiter = ' ';
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void fileUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Title = "Open Text File";
+            file.Filter = "TXT files|*.txt";
+            file.InitialDirectory = @"C:\";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                location.Text = file.FileName;
+                filetext = File.ReadAllText(file.FileName);
+                fileline = File.ReadAllLines(file.FileName);
+                data.Text = filetext;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simulate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ResultLabel.Text = "";
+                matrixLabel.Text = "";
+                if (data.Text != "")
+                {
+                    ListDictionary candidates = new ListDictionary();
+                    int numvoters = fileline.Length;
+                    if (method == 1)
+                    {
+                        numvoters = firstChoice(candidates);
+                    }
+                    else if (method == 2)
+                    {
+                        numvoters = preferenceChoice(candidates);
+                    }
+                    else if (method == 3)
+                    {
+                        numvoters = pairwiseChoice(candidates);
+                    }
+                    updateText(candidates, numvoters);
+                }
+                else
+                {
+                    ResultLabel.Text = "There is no data to simulate";
+                }
+            }
+            catch
+            {
+                ResultLabel.Text = "An error has occurred";
+            }
+        }
+    
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isCollapsed)
+            {
+                dropdownmenu.Height += 10;
+                if (dropdownmenu.Size == dropdownmenu.MaximumSize)
+                {
+                    timer1.Stop();
+                    isCollapsed = false;
+                }
+            }
+            else
+            {
+                dropdownmenu.Height -= 10;
+                if (dropdownmenu.Size == dropdownmenu.MinimumSize)
+                {
+                    timer1.Stop();
+                    isCollapsed = true;
+                }
+            }
+        }
+
+        private void dropdown_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void data_TextChanged(object sender, EventArgs e)
+        {
+            filetext = data.Text;
+            fileline = data.Lines;
+        }
+
+        private void method2_Click(object sender, EventArgs e)
+        {
+            method = 1;
+        }
+
+        private void method3_Click(object sender, EventArgs e)
+        {
+            method = 2;
+        }
+
+        private void pairwise_Click(object sender, EventArgs e)
+        {
+            method = 3;
+        }
+
+        private void delimiterText_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                delimiter = delimiterText.Text[0];
+                error.Text = "delimiter changed.";
+            }
+            catch
+            {
+                delimiter = ' ';
+                error.Text = "delimiter has been defaulted back to a space.";
+            }
+        }
+
+        public int firstChoice(ListDictionary candidates)
+        {
+            //for calculating the percentage of votes later
+            int numvoters = 0;
+
+            //if a candidate is placed first in a row it gets a point
+            foreach (string line in fileline)
+            {
+                string[] votes = line.Split(delimiter);
+                foreach (string vote in votes)
+                {
+                    if (votes[0]!=vote || !amountCheck.Checked)
+                    {
+                        if (!candidates.Contains(vote))
+                        {
+                            candidates.Add(vote, 0);
+                        }
+                    }
+                }
+                if (amountCheck.Checked)
+                {
+                    numvoters = numvoters + Convert.ToInt32(votes[0]);
+                    candidates[votes[1]] = Convert.ToInt32(candidates[votes[1]]) + Convert.ToInt32(votes[0]);
+                }
+                else
+                {
+                    numvoters = fileline.Length;
+                    candidates[votes[0]] = Convert.ToInt32(candidates[votes[0]]) + 1;
+                }
+            }
+            return numvoters;
+        }
+
+        public int preferenceChoice(ListDictionary candidates)
+        {
+            //for calculating the percentage of votes later
+            int numvoters = 0;
+            //gives more points the higher up a candidate is placed
+            foreach (string line in fileline)
+            {
+                string[] votes = line.Split(delimiter);
+                int ii = votes.Length - 1;
+                if (amountCheck.Checked)
+                {
+                    ii--;
+                }
+                for (int i = 0; i < votes.Length; i++)
+                {
+                    int iii = ii;
+                    if (amountCheck.Checked)
+                    {
+                        if (i == 0)
+                        {
+                            i++;
+                        }
+                        iii = ii * Convert.ToInt32(votes[0]);
+                    }
+                    candidates[votes[i]] = Convert.ToInt32(candidates[votes[i]]) + iii;
+                    numvoters = numvoters + iii;
+                    ii--;
+                }
+            }
+            return numvoters;
+        }
+
+
+        public int pairwiseChoice(ListDictionary candidates)
+        {
+            ListDictionary matrix = new ListDictionary();
+
+            int numvoters = 0; //for calculating the percentage of votes later
+
+            //finds all of the possible candidates
+            string[] votes = fileline[0].Split(delimiter);
+            foreach (string vote in votes)
+            {
+                if (votes[0] != vote || !amountCheck.Checked)
+                {
+                    candidates.Add(vote, 0);
+                }
+            }
+
+            //finds all of the possible matchups
+            foreach (string candidate in candidates.Keys)
+            {
+                foreach (string candidate2 in candidates.Keys)
+                {
+                    if(candidate != candidate2)
+                    {
+                        matrix.Add(candidate + "," + candidate2, 0);
+                    }
+                }
+            }
+
+            //For loop through each pair to find who wins against all possible matchups
+            foreach (string line in fileline)
+            {
+                votes = line.Split(delimiter);
+                for (int i = 0; i < votes.Length; i++)
+                {
+                    for (int ii = 0; ii < votes.Length; ii++)
+                    {
+                        if (i < ii)
+                        {
+                            if(amountCheck.Checked)
+                            {
+                                matrix[votes[i] + ',' + votes[ii]] = Convert.ToInt32(matrix[votes[i] + ',' + votes[ii]]) + Convert.ToInt32(votes[0]);
+                            }
+                            else
+                            {
+                                matrix[votes[i] + ',' + votes[ii]] = Convert.ToInt32(matrix[votes[i] + ',' + votes[ii]]) + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            //for printing out the matrix
+            foreach (string candidate in candidates.Keys)
+            {
+                foreach (string candidate2 in candidates.Keys)
+                {
+                    if (candidate != candidate2)
+                    {
+                        matrixLabel.Text = matrixLabel.Text + candidate + ',' + candidate2;
+                        matrixLabel.Text = matrixLabel.Text + ":" + matrix[candidate + ',' + candidate2] + ' ' + ' ';
+                    }
+                }
+                matrixLabel.Text = matrixLabel.Text + "\n";
+            }
+
+            //the number of times a candidate wins against another candidate on the matrix
+            votes = fileline[0].Split(delimiter);
+            foreach (string vote in votes)
+            {
+                foreach (string vote2 in votes)
+                {
+                    if (votes[0] != vote || !amountCheck.Checked)
+                    {
+                        if(Convert.ToInt32(matrix[vote + ',' + vote2])> Convert.ToInt32(matrix[vote2 + ',' + vote]))
+                        {
+                            candidates[vote] = Convert.ToInt32(candidates[vote]) + 1;
+                            numvoters++;
+                        }
+                    }
+                }
+            }
+
+            return numvoters;
+        }
+
+        public int countVotes()
+        {
+            int numvoters = 0;
+
+            foreach (string line in fileline)
+            {
+                string[] votes = line.Split(delimiter);
+                if (amountCheck.Checked)
+                {
+                    numvoters = numvoters + Convert.ToInt32(votes[0]);
+                }
+                else
+                {
+                    numvoters = fileline.Length;
+                }
+            }
+            return numvoters;
+        }
+
+        public void updateText(ListDictionary candidates, int numvoters)
+        {
+            string winner = "";
+            int winnervotes = 0;
+            foreach (DictionaryEntry candidate in candidates)
+            {
+                //you can't divide by zero
+                if (numvoters != 0)
+                {
+                    //percentage of votes
+                    int avg = (Convert.ToInt32(candidate.Value) * 100) / numvoters;
+                    ResultLabel.Text = ResultLabel.Text + candidate.Key + ": " + candidate.Value + " (" + avg.ToString() + "%)" + "\n";
+                }
+                else
+                {
+                    ResultLabel.Text = ResultLabel.Text + candidate.Key + ": " + candidate.Value + " (" + 0 + "%)" + "\n";
+                }
+
+                if (winnervotes < Convert.ToInt32(candidate.Value))
+                {
+                    winner = candidate.Key.ToString();
+                    winnervotes = Convert.ToInt32(candidate.Value);
+                }
+                else if (winnervotes == Convert.ToInt32(candidate.Value))
+                {
+                    winner = winner + ", " + candidate.Key.ToString();
+                }
+            }
+            numvoters = countVotes(); //just so we don't print the number of points allocated and rather just do the number of voters
+            ResultLabel.Text = ResultLabel.Text + "\n\nTotal votes: " + numvoters + "\nWinner:" + winner;
+        }
+    }
+}
